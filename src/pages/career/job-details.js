53878be336b585c5117128acbda33dcb6ed70cf5
Patryk <patrykwojtach@op.pl>
2019-05-24 14:28:20 +0200

    Fixed problem with netlfiy build 2

--- src/pages/career/job-details.js
+++ src/pages/career/job-details.js
@@ -3,7 +3,6 @@ import Navigation from '../../components/navigation';
 import AboutJobList from '../../components/aboutJob';
 
 const JobDetails = ({ location }) => {
-    console.log(location.state.jobId);
     if (location.state) {
         const jobId = location.state.jobId
         return (
