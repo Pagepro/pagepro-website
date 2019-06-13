3e1905e7946908ef3a511e6b791f23ad1a33634d
Patryk <patrykwojtach@op.pl>
2019-06-13 14:35:20 +0200

    Changed components names

--- src/pages/cookies.js
+++ src/pages/cookies.js
@@ -1,6 +1,6 @@
 import React from "react"
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import Header from '../components/header';
+import Footer from '../components/footer';
 import BgImage from '../components/bgImage';
 import { Link } from 'gatsby'
 
